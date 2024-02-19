f9576f8af32916ea1a98b5c43fe8cef22f990aa6
Jonah <kayiwa.jonah@student.moringaschool.com>
2024-02-19 11:35:17 +0300

    reviewed

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+    <div>
+    <header className="App-header">
+    <h1>Welcome to the Royal Bank of Flatiron</h1>
+    </header>
+
+    </div>
+    )
+}
+export default Header;
